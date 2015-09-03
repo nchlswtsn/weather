9f22bdecc4a35b5e9b99b821402f33a9cc99e5e3
Charles Watson <nchlswtsn@gmail.com>
2015-09-03 08:37:14 -0700

    main.js white space removed

--- main.js
+++ main.js
@@ -2,12 +2,9 @@
 
 $(document).ready(init);
 
-
-
 function init() {
   $('#go').click(goClicked);
   $('body').on("keypress", inputEnter);
-
 }
 
 function inputEnter(e) {
@@ -17,10 +14,7 @@ function inputEnter(e) {
 }
 
 function goClicked(e) {
-  console.log('go Clicked');
-
   var $inputCS = $('#inputCS').val().split(", ");
-  console.log($inputCS[0], $inputCS[1]);
   $.get("http://api.wunderground.com/api/daf9b48cc5c4fd1b/conditions/q/" + $inputCS[1] + "/" + $inputCS[0] + ".json", {
     dataType: 'jsonp',
     method: 'GET'
@@ -30,16 +24,11 @@ function goClicked(e) {
     var dateNow = new Date();
     var hourNow = dateNow.getHours();
     var percHour = (hourNow / 24) * 100;
-
-
     var temp = data.current_observation.temp_f
-    // var weatherIcon = "http://www.clipartbest.com/cliparts/dir/ayg/diraygx5T.png"
     var weatherIcon = data.current_observation.icon_url
-    // actual provided icon looked too shitty.
     var full = data.current_observation.display_location.full
     var zip = data.current_observation.display_location.zip
     var time = data.current_observation.local_time_rfc822
-    console.log(percHour);
     $('.progress.progress-striped.active').attr("id", "showBar");
     $('.progress-bar').attr("style", "width: " + percHour + "%");
     $('#query').text(full + " " + zip + ", " + time);
@@ -48,13 +37,8 @@ function goClicked(e) {
     $('#outputTemp').text(temp + "°F");
     $('#two').text("Conditions:");
     $('#icon').attr("src", weatherIcon);
-    console.log($('.progress-bar'));
-    console.log('data:', data);
-    console.log($('#icon'));
   })
   .fail(function(error) {
     console.log('error:', error);
-
   });
-
 }
